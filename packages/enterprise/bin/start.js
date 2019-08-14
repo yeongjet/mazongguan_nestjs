437842ce27c11261ffed94f06302661782c05861
yeongjet <625436194@163..com>
2019-08-15 01:37:39 +0800

    update

--- packages/enterprise/bin/start.js
+++ packages/enterprise/bin/start.js
@@ -10,10 +10,10 @@ new APIEnterprise({
     },
     docUrl: '/api/v1/doc',
     database: {
-        host: process.env.POSTGRES_HOST || 'localhost',
-        port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-        username: process.env.POSTGRES_USER || 'lxdhub',
-        password: process.env.POSTGRES_PASSWORD || 'lxdhub',
-        database: process.env.POSTGRES_DATABASE || 'lxdhub'
+        host: 'localhost',
+        port: 5432,
+        username: 'yeongjet',
+        password: 'qwe123',
+        database: 'mzgdev'
     }
 }).run()
