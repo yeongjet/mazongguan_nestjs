d4e3ccbd649a7369a3d9a6214683dbfe2662c9c0
Denney <625436194@163.com>
2019-08-15 11:57:51 +0800

    update

---
+++ packages/enterprise/bin/start.ts
@@ -0,0 +1,23 @@
+import { APISetting, LogLevel, DBSetting, DocSetting } from '@mazongguan/common'
+import { APIEnterprise } from '../src'
+const option: APISetting = {
+    port: 3000,
+    host: 'localhost',
+    logLevel: LogLevel.LOG,
+    database: {
+        host: 'localhost',
+        username: 'yeongjet',
+        password: 'qwe123',
+        database: 'mzgdev',
+        port: 5432,
+        logging: true
+    },
+    doc: {
+        title: 'Mazongguan Enterprise API',
+        description: '码总管企业端文档',
+        version: '1.0',
+        url: '/api/enterprise'
+    }
+}
+
+new APIEnterprise(option).start()
