62fd00464d673cf07ea61e25099c8971c67e61db
Denney <625436194@163.com>
2019-08-15 16:31:37 +0800

    'update'

--- packages/common/src/class/APISetting.class.ts
+++ packages/common/src/class/APISetting.class.ts
@@ -8,9 +8,6 @@ export class APISetting {
     @IsString()
     @Type(() => String)
     protocol
-    @IsString()
-    @Type(() => String)
-    protocol2
     @IsInt()
     @Type(() => Number)
     port
